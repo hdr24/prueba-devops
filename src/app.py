0626843aa11b233ecc4479b9ee36ebf739f39d05
Hector del Real <hrealop982@g.educaand.es>
2024-03-07 09:19:37 +0100

    Actualizada app.py

--- src/app.py
+++ src/app.py
@@ -5,21 +5,16 @@ import locale
 app = Flask(__name__)
 locale.setlocale(locale.LC_TIME, '')
 
-
 @app.route('/')
 def homepage():
-    x = "variable no es usada para error linter"
     the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
 
     # Render HTML with variable
     return render_template("index.html", the_time=the_time, tema="cats")
 
-
-
 @app.route('/status')
 def status():
     return "OK Todo"
-
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
